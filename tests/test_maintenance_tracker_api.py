0f6b98cd59db98c8e290ef1b051d4746587ffbcb
Edwin Moses <workockmoses@gmail.com>
2018-05-30 10:46:20 +0300

    test for returning a request and creating a request

--- tests/test_maintenance_tracker_api.py
+++ tests/test_maintenance_tracker_api.py
@@ -16,45 +16,35 @@ class MaintenanceTrackerTestCase(unittest.TestCase):
 		self.app = create_app(config_name="testing")
 		self.client = self.app.test_client
 
-	def test_maintenance_tracker_can_get_a_request(self):
-		"""
-		Test API can return single request item
-		"""
-		res = self.client().get("/users/api/v1.0/requests/{}".format(2))
-		self.assertEqual(res.status_code, 200)
-		self.assertIn("title", str(res.data))
-
-	def test_maintenance_tracker_api_can_get_all_requests(self):
-		"""
-		Test API can return all the requests
-		"""
-		res = self.client().get("/users/api/v1.0/requests/")
-		self.assertEqual(res.status_code, 200)
-		self.assertEqual(len(str(res.data)) == 1, msg="Should be alist of length 1")
-
-	def test_maintenance_tracker_api_can_edit_a_request(self):
-		"""
-		Test APi can edit a single request
-		"""
-		res = self.client().put("/users/api/v1.0/requests/{}".format(2), {"approved": False})
-		self.assertEqual(res.status_code, 200)
-		rev = self.client().get("/users/api/v1.0/requests/{}".format(2))
-		self.assertEqual(rev.status_code, 200)
-		self.assertTrue(str(res.data["approved"]), msg="Approved should be True")
-
-	def test_maintenance_tracker_api_can_create_requests(self):
+	def test_api_can_create_a_request(self):
 		"""
 		Test API can create a request
 		"""
-		res = self.client().post("/users/api/v1.0/requests/", {
-			"id": 3,
-			"title": "Broken tap",
-			"description": "Overflow due to a broken tap. Can't close",
-		})
+		res = self.client().post("/users/api/v1.0/requests/", json=dict(
+			title="Busted pipe",
+			description="I have a leaking pipe in my sink and i have children in the house",
+			location="Kisumu",
+			create_by="Mike"
+		))
 		self.assertEqual(res.status_code, 201)
-		rev = self.client().get("/users/v1.0/api/requests/{}".format(3))
+		self.assertTrue(res.data)
+
+	def test_api_can_return_all_requests(self):
+		"""
+		Test API can return all requests created by a user
+		"""
+		res = self.client().get("/users/api/v1.0/requests/")
 		self.assertEqual(res.status_code, 200)
-		self.assertTrue(str(res.data), msg="Response data should not be empty")
+		self.assertTrue(str(res.data))
+
+	def test_api_can_return_a_request_create_by_a_user(self):
+		"""
+		Test API can return a request created by a user
+		"""
+		res = self.client().post("/users/api/v1.0/requests/{}/".format(2))
+		self.assertEqual(res.status_code, 200)
+		self.assertTrue(str(res.data))
+
 
 if __name__ == '__main__':
 	unittest.main()
