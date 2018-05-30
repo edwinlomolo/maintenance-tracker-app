58d3a338ab220e021fec2fa1dd9b93767775894d
Edwin Moses <workockmoses@gmail.com>
2018-05-30 11:24:29 +0300

    create test for admin endpoints and user accounts endpoints

--- tests/test_maintenance_tracker_api.py
+++ tests/test_maintenance_tracker_api.py
@@ -16,6 +16,31 @@ class MaintenanceTrackerTestCase(unittest.TestCase):
 		self.app = create_app(config_name="testing")
 		self.client = self.app.test_client
 
+	def test_api_can_create_user_account(self):
+		"""
+		Test API can create user account
+		"""
+		res = self.client().post("/users/api/v1.0/account/register/", json=dict(
+			firstname="Edwin",
+			lastname="Lomolo",
+			email="edwin@gmail.com",
+			password=1234,
+			confirm_password=1234
+		))
+		self.assertEqual(res.status_code, 201)
+		self.assertTrue(str(res.data))
+
+	def test_api_can_log_in_user(self):
+		"""
+		Test API can login user
+		"""
+		res = self.client().post("/users/api/v1.0/authenticate/", json=dict(
+			email="edwin@gmail.com",
+			password=1234
+		))
+		self.assertEqual(res.status_code, 200)
+		self.assertTrue(str(res.data))
+
 	def test_api_can_create_a_request(self):
 		"""
 		Test API can create a request
@@ -33,7 +58,7 @@ class MaintenanceTrackerTestCase(unittest.TestCase):
 		"""
 		Test API can return all requests created by a user
 		"""
-		res = self.client().get("/users/api/v1.0/requests/")
+		res = self.client().get("/users/api/v1.0/requests/", headers=dict(name="Edwin"))
 		self.assertEqual(res.status_code, 200)
 		self.assertTrue(str(res.data))
 
@@ -41,7 +66,7 @@ class MaintenanceTrackerTestCase(unittest.TestCase):
 		"""
 		Test API can return a request created by a user
 		"""
-		res = self.client().post("/users/api/v1.0/requests/{}/".format(2))
+		res = self.client().post("/users/api/v1.0/requests/{}/".format(2), headers=dict(name="Edwin"))
 		self.assertEqual(res.status_code, 200)
 		self.assertTrue(str(res.data))
 
@@ -49,7 +74,15 @@ class MaintenanceTrackerTestCase(unittest.TestCase):
 		"""
 		Test API can return requests for admin
 		"""
-		res = self.client().get("/admin/api/v1.0/requests/")
+		res = self.client().get("/admin/api/v1.0/requests/", headers=dict(name="admin"))
+		self.assertEqual(res.status_code, 200)
+		self.assertTrue(str(res.data))
+
+	def test_api_can_edit_a_request_for_admin(self):
+		"""
+		Test API can allow request edit for admin
+		"""
+		res = self.client().post("/admin/api/v1.0/requests/{}/".format(1), headers=dict(name="admin"))
 		self.assertEqual(res.status_code, 200)
 		self.assertTrue(str(res.data))
 
