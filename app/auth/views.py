906bda7059ea704afcc6ea8b83a3ca5074ff9423
Edwin Moses <workockmoses@gmail.com>
2018-06-06 20:49:08 +0300

    Feat:create signup endpoint

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,7 +3,7 @@ Registration view
 """
 import psycopg2
 
-from flask import make_response, request, jsonify, abort
+from flask import make_response, request, jsonify
 from flask.views import MethodView
 from models.user import User
 
@@ -17,30 +17,41 @@ class Registration(MethodView):
         """
         Handle post requests on this view
         """
-        email = request.json.get('email', '')
-        user = User.query(email)
+        if request.json:
+            email = request.json.get('email', '')
+            password = request.json.get('password', '')
 
-        if user is None:
-            return jsonify({ "massage": "User not found"})
-        try:
-            conn = psycopg2.connect(
-                host="localhost",
-                database="mtapi",
-                user="edwin",
-                password="47479031"
-            )
-            query = """SELECT EMAIL FROM USERS WHERE EMAIL = %s """
+            try:
+                conn = psycopg2.connect(
+                    host="localhost",
+                    database="mtapi",
+                    user="edwin",
+                    password="47479031"
+                )
 
-            cur = conn.cursor()
-            cur.execute(query, (email,))
-            row = cur.fetchone()
+                query = """SELECT EMAIL FROM USERS WHERE EMAIL = %s"""
 
-            if row is not None:
-                return jsonify(row)
-            return jsonify({ "message": "Empty data" })
-        except(Exception, psycopg2.DatabaseError) as error: # pylint: disable=broad-except
-            return jsonify({ "message": str(error) })
+                cur = conn.cursor()
+                cur.execute(query, (email,))
 
+                row = cur.fetchone()
+
+                if row is not None:
+                    return make_response(jsonify({
+                        "error": "Email already taken. Please choose a different one."
+                    })), 200
+                user = User(email=email, password=password)
+                user.save()
+                return make_response(jsonify({
+                    "message": "You are successfully registered. You can log in now."
+                })), 201
+
+            except(Exception, psycopg2.DatabaseError) as error: # pylint: disable=broad-except
+                return make_response(jsonify({"message": str(error)}))
+        else:
+            return make_response(jsonify({"error": "Invalid input"})), 400
+
+# API Resource
 SIGN_UP = Registration.as_view("signup_view")
 AUTH_BLUEPRINT.add_url_rule(
     "/api/v1.0/auth/signup/",
