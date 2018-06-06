c298ce64f460d619c495cdead7e36963af30cd7c
Edwin Moses <workockmoses@gmail.com>
2018-06-07 01:33:24 +0300

    feat(user signin): create user signin endpoint

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,7 @@
 """
 Registration view
 """
+import os
 import psycopg2
 
 from flask import make_response, request, jsonify
@@ -68,8 +69,46 @@ class Login(MethodView):
         Handles post requests on this view
         """
         if request.json and request.json.get('email') and request.json.get('password'):
-            user = User.get_user(request.json.get('email'))
-            return jsonify(user)
+
+            # Get data from request
+            email = request.json.get('email')
+            password = request.json.get('password')
+
+            # Try connecting to database
+            try:
+                conn = psycopg2.connect(
+                    host=os.getenv("HOST"),
+                    database=os.getenv("DB"),
+                    user=os.getenv("USER"),
+                    password=os.getenv("PASS")
+                )
+                query = """SELECT ID, EMAIL, PASSWORD FROM USERS WHERE EMAIL = %s"""
+
+                cur = conn.cursor()
+                cur.execute(query, (email,))
+
+                row = cur.fetchone()
+
+                # Validate password
+                if row is not None and User.validate_password(row[2], password):
+                    # Send back token
+                    return make_response(jsonify({
+                        "message": "Logged in successfully.",
+                        "token": User.generate_token(row[0])
+                    })), 200
+                # If wrong password or incorrect email address
+                return make_response(jsonify({
+                    "error": "Invalid email or password."
+                })), 401
+            except(Exception, psycopg2.DatabaseError) as error: # pylint: disable=broad-except
+                return make_response(jsonify({
+                    "error": str(error)
+                })), 500
+        else:
+            # If request is empty
+            return make_response(jsonify({
+                "message": "Please provide both a valid email and password to log in."
+            }))
 
 # Define Signup Resource
 SIGN_UP = Registration.as_view("signup_view")
