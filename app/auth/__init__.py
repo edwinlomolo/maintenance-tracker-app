f5540ac60a9810c91a2a8332b2bd6733bdb47441
Edwin Moses <workockmoses@gmail.com>
2018-06-06 20:51:56 +0300

    Chore(setup pylint): order imports

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -2,7 +2,8 @@
 Authentication Blueprint
 """
 from flask import Blueprint
-from . import views
 
 # instance of blueprint that instantiate authentication blueprint
 AUTH_BLUEPRINT = Blueprint("auth", __name__)
+
+from . import views # pylint: disable=wrong-import-position
