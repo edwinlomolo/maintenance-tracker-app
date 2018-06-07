f3de878a49f8e3104b6d6915a3bdcf835e9a0cbc
Edwin Moses <workockmoses@gmail.com>
2018-06-07 08:18:45 +0300

    chore(write tests):test auth endpoints error handling techniques

--- tests/test_user.py
+++ tests/test_user.py
@@ -37,6 +37,57 @@ class AuthenticationTestCases(unittest.TestCase):
             str(result["message"]), "You have successfully registered, You can now log in."
         )
 
+    def test_account_duplication(self):
+        """
+        Test API can handle email duplication error
+        """
+        res = self.client().post(
+            "/api/v1.0/auth/signup/",
+            json=dict(email="johndoe@email.com", password="4747")
+        )
+        result = json.loads(res.data.decode())
+        self.assertEqual(res.status_code, 202)
+        self.assertEqual(
+            str(result["message"]), "Email already taken. Please choose a different one."
+        )
+
+    def test_signup_input(self):
+        """
+        Test API can handle empty requests during signup
+        """
+        res = self.client().post("/api/v1.0/auth/signup/")
+        result = json.loads(res.data.decode())
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(str(result["error"]), "Please provide both an email and a password.")
+
+    def test_email_signup(self):
+        """
+        Test API can handle orphaned email input during signup
+        """
+        res = self.client().post(
+            "/api/v1.0/auth/signup/",
+            json=dict(email="johndoe@email.com")
+        )
+        result = json.loads(res.data.decode())
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(
+            str(result["error"]), "Please provide both an email and a password."
+        )
+
+    def test_password_signup(self):
+        """
+        Test API can handle orphaned password input during signup
+        """
+        res = self.client().post(
+            "/api/v1.0/auth/signup/",
+            json=dict(password="4747")
+        )
+        result = json.loads(res.data.decode())
+        self.assertEqual(res.status_code, 202)
+        self.assertEqual(
+            str(result["error"]), "Please provide both an email and a password."
+        )
+
     def test_user_can_log_in(self):
         """
         Test API can authenticate user
@@ -50,7 +101,7 @@ class AuthenticationTestCases(unittest.TestCase):
         self.assertEqual(
             str(result["message"]), "You logged in successfully."
         )
-        self.assertIn(result["token"], "eY")
+        self.assertIn("eY", str(result["token"]))
 
     def test_unauthorized_access(self):
         """
@@ -63,3 +114,40 @@ class AuthenticationTestCases(unittest.TestCase):
         result = json.loads(res.data.decode())
         self.assertEqual(res.status_code, 401)
         self.assertEqual(result["error"], "Invalid password or email.")
+
+    def test_signin_input(self):
+        """
+        Test API can handle empty requests during signin
+        """
+        res = self.client().post("/api/v1.0/auth/signin/")
+        result = json.loads(res.data.decode())
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(str(result["error"]), "Invalid email or password.")
+
+    def test_orphaned_email(self):
+        """
+        Test API can handle orphaned email input during signin
+        """
+        res = self.client().post(
+            "/api/v1.0/auth/signin/",
+            json=dict(email="johndoe@email.com")
+        )
+        result = json.loads(res.data.decode())
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(
+            str(result["error"]), "Invalid email or password."
+        )
+
+    def test_orphaned_password(self):
+        """
+        Test API can handle orphaned password input
+        """
+        res = self.client().post(
+            "/api/v1.0/auth/signup/",
+            json=dict(password="4747")
+        )
+        result = json.loads(res.data.decode())
+        self.assertEqual(res.status_code, 202)
+        self.assertEqual(
+            str(result["error"]), "Invalid email or password."
+        )
