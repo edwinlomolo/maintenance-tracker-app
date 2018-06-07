506187fb2fa27c76ecbe39e34e8b5ab06abda4d0
Edwin Moses <workockmoses@gmail.com>
2018-06-08 00:11:52 +0300

    feat(user model): add email validator

--- app/auth/views.py
+++ app/auth/views.py
@@ -6,6 +6,7 @@ import psycopg2
 from flask import make_response, request, jsonify
 from flask.views import MethodView
 from models.user import User
+from validate_email import validate_email
 
 from . import AUTH_BLUEPRINT
 
@@ -22,36 +23,43 @@ class Registration(MethodView):
             # Get data from body
             email = request.json.get('email')
             password = request.json.get('password')
+            is_valid = validate_email(email)
 
-            # Try connecting to the database
-            try:
-                conn = psycopg2.connect(
-                    host=os.getenv("HOST"),
-                    database=os.getenv("DB"),
-                    user=os.getenv("USER"),
-                    password=os.getenv("PASS")
-                )
-                query = """SELECT EMAIL FROM USERS WHERE EMAIL = %s"""
+            if is_valid:
+                # Try connecting to the database
+                try:
+                    conn = psycopg2.connect(
+                        host=os.getenv("HOST"),
+                        database=os.getenv("DATABASE"),
+                        user=os.getenv("USER"),
+                        password=os.getenv("PASS")
+                    )
+                    query = """SELECT EMAIL FROM USERS WHERE EMAIL = %s"""
 
-                # Get cursor object
-                cur = conn.cursor()
-                cur.execute(query, (email,))
+                    # Get cursor object
+                    cur = conn.cursor()
+                    cur.execute(query, (email,))
 
-                row = cur.fetchone()
+                    row = cur.fetchone()
 
-                if row is not None:
+                    if row is not None:
+                        return make_response(jsonify({
+                            "message": "Email already taken. Please choose a different one."
+                        })), 202
+                    user = User(email=email, password=password)
+                    user.save()
                     return make_response(jsonify({
-                        "message": "Email already taken. Please choose a different one."
-                    })), 202
-                user = User(email=email, password=password)
-                user.save()
+                        "message": "You successfully registered. You can log in now."
+                    })), 201
+                except(Exception, psycopg2.DatabaseError) as error: # pylint: disable=broad-except
+                    return make_response(jsonify({
+                        "error": str(error)
+                    })), 500
+            else:
                 return make_response(jsonify({
-                    "message": "You successfully registered. You can log in now."
-                })), 201
-            except(Exception, psycopg2.DatabaseError) as error: # pylint: disable=broad-except
-                return make_response(jsonify({
-                    "error": str(error)
-                })), 500
+                    "error": "Invalid email address. Provide a valid email address."
+                })), 400
+
         else:
             # If both email and password are not provided
             return make_response(jsonify({
